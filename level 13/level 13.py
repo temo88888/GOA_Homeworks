4d07ae46ccf009d8bec94ccee2377bfcc53f13ee
temo88888 <Temokhveladze@gmail.com>
2024-06-23 17:28:41 +0400

    commit

---
+++ level 13/level 13.py
@@ -0,0 +1,49 @@
+#დავალება N1
+x = int(input("what is the first number?> "))
+y = int(input("what is the second number?> "))
+g = int(input("what is the third number?> "))
+h = int(input("what is the fourth number?> "))
+
+print(y * x)
+print(y / x)
+print(g + h)
+print(g - h)
+
+#დავალება N2
+my_age = int(input("how old are you?> "))
+my_sisters_age =int(input("how old is your siters?> "))
+
+print(my_age + 20)
+print(my_sisters_age + 20)
+
+#დავალება N3
+name ="Temo"
+surname ="qveladze"
+age =10
+country ="Georgia"
+hobby ="swimming"
+
+
+print(f"My name is {name}. My surname is {surname}. I am {age} years old. I am from {country}. My hobby is {hobby}.")
+# fstring - formated string
+
+# დავალება N4
+x = input('what is the name?> ')
+
+
+print(f"Happy birthday to {x}. I wish you everything best.")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
