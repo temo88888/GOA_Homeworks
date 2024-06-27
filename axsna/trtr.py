5378e2e0bc061513218bdc48708f82f45282a855
temo88888 <Temokhveladze@gmail.com>
2024-06-27 15:01:17 +0400

    commit

--- axsna/trtr.py
+++ axsna/trtr.py
@@ -1,10 +1,10 @@
-for i in range(20):
-    print("მე მიყვარს იმერული ხაჭაპური")
+# for i in range(20):
+#     print("მე მიყვარს იმერული ხაჭაპური")
 
-#ახსნა 2
+# #ახსნა 2
 
-for i in range(10):
-    print(i)
+# for i in range(10):
+#     print(i)
 
 
 #ახსნა 3
@@ -12,13 +12,66 @@ for i in range(10):
 # :-colon symbol  (ორი წერტილი ნიშნავს ჩამოთვლას)
 
 #While ლუპები ბეჭდავენ(print)იქამდე სანამ პირობა არის მართალი
-seats =100
-while seats > 0:
-    print('There are seats left!')
-    seats = seats - 1
+# # seats =100
+# while seats > 0:
+#     print('There are seats left!')
+#     seats = seats - 1
 
 # ახსნა 4
 #counter ამბობს რომ არმდენჯერ უნდა დაპრინტოს
+
+
+# axalia
+# 1) Sequencing means that the computer runs your code in order, from top to bottom.
+# ანუ კომპიუტერი მიჰყვება შენს კოდს თანმიმდევრობის მიხედვით(რიგრიგობით)
+
+# print("Hello")
+# print("Hi")
+# print("Goodbye")
+# print("Bye")
+# print("ByeBye")
+
+# 2) Iteration is about executing an instruction repeatedly. Iteration is commonly represented as a loop. (for loop…)
+# ანუ კოდის განმეორებით შესრულება (შეიძლება იყოს უსასრულო) მაგალითები: for loop, while loop.
+
+# for i in range(500):
+#     print("GOA is the best")
+
+#or
+
+# while True:
+#     print("Hello") #Don't run
+
+
+
+#უსასრულო კოდი
+
+# 3) Selection specifies when to follow each path. (if loop…)
+#ანუ კომპიუტერი იღებს ლოგიკურ განდაწყვეთილებას მაგალითად: if loop
+
+# age = 12
+
+# if age > 14:
+#     print("You are older than 14!")
+# elif age < 14:
+#     print("You are younger that 14!")
+# elif age == 14:
+#     print("You are 14!")
+
+
+# !11111
+
+
+# avsatfdtfi
+# x = int(input("what is the first number?> "))
+# 1
+
+
+
+
+
+
+
 
 
 
