99ffe4dcbd6c28206d27cb030b3a0b44664ab2cc
temo88888 <Temokhveladze@gmail.com>
2024-06-29 14:24:30 +0400

    commit

---
+++ level 15,16,17,18,19,20/klas vork.py
@@ -0,0 +1,4 @@
+sted = int(imput())#1213
+acive_minutes = int(imput())#41
+print =( sted> acive miunutes)
+print = (true or false)
---
+++ levl 15/level 15.py
@@ -0,0 +1,69 @@
+# დავალება N1
+a =303
+b =213
+b < a
+print (True or False)
+
+e =12
+q =11
+r =30
+w =60
+print (e < r and w > q)
+
+o =333
+i = 4545
+o > i
+print (True and False)
+
+g =334
+h =443
+f =222
+j =111
+print (g < j or f > h)
+
+y =231
+z =232
+print( y < z or z < y)
+# .........
+m =o
+m >1
+print (True or False)
+
+u =12
+s =11
+l =30
+ol =60
+print (u < ol and l > s)
+
+qe =333
+fr = 4545
+qe > fr
+print (True and False)
+
+qw =334
+er =443
+ty =222
+ui =111
+print (qw < er or ty > ui)
+
+cv =234
+tv =232
+print( cv < tv )
+
+
+# დავალება N2
+steps_taken = int(input("Enter the number of steps you have taken in 10 minutes> "))
+
+has_taken_enough_steps = steps_taken >= 1000
+
+print(has_taken_enough_steps)
+
+
+
+
+
+# დავალება N3
+
+# The and operator returns True if both conditions are True, otherwise it returns False.
+# The or operator returns True if at least one of the conditions is True, otherwise it returns False.
+
